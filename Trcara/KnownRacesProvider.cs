97425d8a9fb0844dc87cccac03d3fdf45b2daa36
Aleksey Kozlov <akozlov@venn.com>
2026-01-01 15:21:45 +0100

    handle quotes properly

--- Trcara/KnownRacesProvider.cs
+++ Trcara/KnownRacesProvider.cs
@@ -2,7 +2,9 @@
 
 internal static class KnownRacesProvider
 {
-    private const string TrcaraColumnBExport = @"https://docs.google.com/spreadsheets/d/1o3LivaIhBS0M1_bG9H8Pq_9K57AVFo0H40h0MzCOICs/gviz/tq?tqx=out:csv&tq=select%20B";
+    private const string TrcaraColumnBExport = "https://docs.google.com/spreadsheets/d/1o3LivaIhBS0M1_bG9H8Pq_9K57AVFo0H40h0MzCOICs/gviz/tq?tqx=out:csv&tq=select%20B";
+    // https://chatgpt.com/share/6956818c-713c-8005-ae06-2c265e11737a
+
 
     public static async Task<string[]> GetKnownRunsAsync()
     {
@@ -13,7 +15,12 @@ internal static class KnownRacesProvider
             var httpClient = new HttpClient();
             var html = await httpClient.GetStringAsync(TrcaraColumnBExport);
 
-            var knownRaces = html.Split('\n').Select(s => s.Trim('"')).Skip(1).ToArray();
+            var knownRaces = html.Split('\n')
+                .Skip(1) // Название
+                .Where(s => !string.IsNullOrWhiteSpace(s))
+                .Select(s => s.Trim().Replace("\"\"", "\""))
+                .Select(s => s.Substring(1, s.Length - 2))
+                .ToArray();
 
             Console.WriteLine($"\n{knownRaces.Length} races are known. Starting from '{knownRaces.FirstOrDefault()}' to '{knownRaces.LastOrDefault()}'\n");
 
